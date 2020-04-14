e9dff4d01c245f8a441e7c15627ebf8dfe29951a
Luca Melandri <lucamelandri0@gmail.com>
2020-04-14 12:23:46 +0200

    First commit, set generic data structure implementation

---
+++ set.go
@@ -0,0 +1,33 @@
+// Package set implements set data structure
+package set
+
+// Any type define an alias for the generic interface{} type
+type Any interface{}
+
+// Set struct implements set data structure for any type
+type Set struct {
+	elements map[Any]Any
+}
+
+// NewSet function creates a set and returns it
+func NewSet() *Set {
+	s := &Set{}
+	s.elements = make(map[Any]Any)
+	return s
+}
+
+// Add function add an item to the set
+func (s *Set) Add(value Any) {
+	s.elements[value] = nil
+}
+
+// Remove function remove an items from the set
+func (s *Set) Remove(value Any) {
+	delete(s.elements, value)
+}
+
+// Contains function check if the set data structure contains the value element
+func (s *Set) Contains(value Any) bool {
+	_, c := s.elements[value]
+	return c
+}
